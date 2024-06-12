13e542143a57b1086aa651b8bb92df26aaa1961d
svtter <svtter@qq.com>
2024-06-12 10:48:32 +0800

    use pdm for this project

--- src/git_record/git_record.py
+++ src/git_record/git_record.py
@@ -4,8 +4,7 @@ from git import Repo
 
 
 class GitHandle:
-
-    def __init__(self, remote_name='origin'):
+    def __init__(self, remote_name="origin"):
         git_repo_dir = os.getcwd()
         self.repo = Repo(git_repo_dir)
         self.remote_name = remote_name
@@ -20,14 +19,12 @@ class GitHandle:
         Through this method to create new branch and save the source code to repo.
         """
         assert not self.repo.is_dirty()
-        branch_name = arrow.now().format('YYYY-MM-DD-HH-mm-ss')
+        branch_name = arrow.now().format("YYYY-MM-DD-HH-mm-ss")
         os.system(f'git checkout -b "{branch_name}"')
-        os.system(f'git push {self.remote_name} {branch_name}')
-        os.system(f'git checkout master')
+        os.system(f"git push {self.remote_name} {branch_name}")
+        os.system(f"git checkout master")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     gh = GitHandle()
     gh.create_new_branch()
-
-
--- tasks.py
+++ tasks.py
@@ -2,18 +2,5 @@ from invoke import task
 
 
 @task
-def commit(c):
-    c.run('git add .')
-    msg = input("Input your commit msg: ")
-    c.run(f'git commit -m "{msg}"')
-    c.run('git push')
-
-
-@task
-def build(c):
-    c.run("python setup.py sdist bdist_wheel")
-
-
-@task
 def upload(c):
     c.run("python -m twine upload dist/*")
